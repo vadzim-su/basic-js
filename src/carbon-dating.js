7e5cebf32c92a9ebde9147fedc9e3562d83ed65a
vadzim-su <vadimmm0991@gmail.com>
2020-03-14 00:26:44 +0300

    feat: add solution of carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity == 'string' && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) <= 15) {
   let ratio = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))
-    return parseInt(HALF_LIFE_PERIOD * ratio / Math.LN2)
+    return Math.ceil(HALF_LIFE_PERIOD * ratio / 0.693)
   }
   else {
   return false
